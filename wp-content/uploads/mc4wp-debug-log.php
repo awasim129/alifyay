39a8e6d9f0fe1d81ff50699ab19b7896cb0cf82d
awasim129 <anas@alifyay.pk>
2017-02-26 09:50:17 +0000

    Banners + New Uploads

--- wp-content/uploads/mc4wp-debug-log.php
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -8,3 +8,4 @@
 [2017-02-19 15:54:41] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
 [2017-02-19 15:55:35] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
 [2017-02-19 15:58:13] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
+[2017-02-21 00:11:08] WARNING: Form 3189 > awas*****@gm***.com is already subscribed to the selected list(s)
