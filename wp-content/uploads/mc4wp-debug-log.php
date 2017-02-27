4d55e56ad39ba68085366ab29e65baa758a02c6c
yahyaw786 <yahyawasim@live.com>
2017-02-27 12:17:14 +0500

    New Features added (Share buttons, variation thumbnails in pictures)and fixes(Zoom fix, padding)

--- wp-content/uploads/mc4wp-debug-log.php
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -9,3 +9,6 @@
 [2017-02-19 15:55:35] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
 [2017-02-19 15:58:13] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
 [2017-02-21 00:11:08] WARNING: Form 3189 > awas*****@gm***.com is already subscribed to the selected list(s)
+[2017-02-26 19:12:55] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
+[2017-02-26 19:13:09] ERROR: Form 3189 > MailChimp API error: 404 Not Found. The requested resource could not be found.
+[2017-02-26 19:19:16] ERROR: Form 3189 > MailChimp API error: 0 cURL error 28: Resolving timed out after 10536 milliseconds.
